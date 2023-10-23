51324c9765d69ccff396235de76c070453ae4810
Ben <44266406@qq.com>
2023-10-23 09:43:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#update syslink
+rm -f staging_dir/host/.prereq-build
+make prereq
