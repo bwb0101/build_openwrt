66f775f757e0d121ebf79eee8d44b97f2591778a
Ben <44266406@qq.com>
2024-10-16 16:24:59 +0800

    Update del_default_packages.sh

--- del_default_packages.sh
+++ del_default_packages.sh
@@ -1,5 +1,5 @@
-sed -i "s/luci-app-adbyby-plus //g" `grep luci-app-adbyby-plus -rl --include="Makefile" ./target/linux/`
-sed -i "s/luci-app-nlbwmon //g" `grep luci-app-nlbwmon -rl ./include`
+#sed -i "s/luci-app-adbyby-plus //g" `grep luci-app-adbyby-plus -rl --include="Makefile" ./target/linux/`
+#sed -i "s/luci-app-nlbwmon //g" `grep luci-app-nlbwmon -rl ./include`
 #sed -i "s/luci-app-zerotier //g" `grep luci-app-zerotier -rl --include="Makefile" ./target/linux/`
 #sed -i "s/autosamba //g" `grep autosamba -rl --include="Makefile" ./target/linux/`
 #sed -i "s/luci-app-wireguard //g" `grep luci-app-wireguard -rl --include="Makefile" ./target/linux/`
