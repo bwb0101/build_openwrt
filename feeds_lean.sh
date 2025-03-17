1af9d0bb4fac29e9d23b4c76af6d0da37579b2be
Ben <44266406@qq.com>
2025-03-17 19:14:09 +0800

    Update feeds_lean.sh

--- feeds_lean.sh
+++ feeds_lean.sh
@@ -5,7 +5,7 @@ echo ""  >> feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
-echo "src-git ssrp https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
 echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
+#mbedtls与openssl冲突 echo "src-git ssrp https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
 #echo "src-git bypass https://github.com/kiddin9/openwrt-bypass.git;main" >>  feeds.conf.default
 #echo "src-git subconverter https://github.com/tindy2013/openwrt-subconverter.git;master" >>  feeds.conf.default
