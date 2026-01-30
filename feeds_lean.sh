cb1ef57ff4c39e5b0a028815f1e3c4d926525c5f
Ben <5220242+bwb0101@users.noreply.github.com>
2026-01-30 23:20:01 +0800

    Update feeds_lean.sh

--- feeds_lean.sh
+++ feeds_lean.sh
@@ -2,11 +2,10 @@
 sed -i 's#https://github.com/coolsnowwolf/packages#https://github.com/jkingben/build_openwrt.git;packages_lean#' feeds.conf.default
 sed -i 's#https://github.com/coolsnowwolf/luci.git;openwrt-24.10#https://github.com/jkingben/build_openwrt.git;luci_lean#' feeds.conf.default
 echo ""  >> feeds.conf.default
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> feeds.conf.default
+echo "src-git passwall2_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> feeds.conf.default
 echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
-echo "src-git argon https://github.com/jerrykuku/luci-theme-argon.git;master" >> feeds.conf.default
 #mbedtls与openssl冲突 echo "src-git ssrp https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
 #echo "src-git bypass https://github.com/kiddin9/openwrt-bypass.git;main" >>  feeds.conf.default
 #echo "src-git subconverter https://github.com/tindy2013/openwrt-subconverter.git;master" >>  feeds.conf.default
