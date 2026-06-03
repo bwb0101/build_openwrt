ed573d97855d077c0ee969d248fbf3a6a80c5f90
Ben <5220242+bwb0101@users.noreply.github.com>
2026-06-03 19:59:15 +0800

    Update feeds_lean.sh

--- feeds_lean.sh
+++ feeds_lean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i 's#https://github.com/coolsnowwolf/packages#https://github.com/jkingben/build_openwrt.git;packages_lean#' feeds.conf.default
-sed -i 's#https://github.com/coolsnowwolf/luci.git;openwrt-24.10#https://github.com/jkingben/build_openwrt.git;luci_lean#' feeds.conf.default
+sed -i 's#https://github.com/coolsnowwolf/luci.git;openwrt-25.12#https://github.com/jkingben/build_openwrt.git;luci_lean#' feeds.conf.default
 echo ""  >> feeds.conf.default
 echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> feeds.conf.default
