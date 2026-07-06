c1d2d4ecc4b5afb2af08d0f3f80c0c6f6d272346
Ben <5220242+bwb0101@users.noreply.github.com>
2026-07-07 00:03:23 +0800

    Create feeds_immortalwrt.sh

---
+++ feeds_immortalwrt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "src-git easytier https://github.com/EasyTier/luci-app-easytier.git;main" >> feeds.conf.default
+echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5" >> feeds.conf.default
+echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> feeds.conf.default
