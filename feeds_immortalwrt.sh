79801b67d51c1d78e944b485cd1b6f0dd8b2274a
Ben <5220242+bwb0101@users.noreply.github.com>
2026-07-07 23:56:02 +0800

    Update feeds_immortalwrt.sh

--- feeds_immortalwrt.sh
+++ feeds_immortalwrt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sed -i 's#https://github.com/immortalwrt/packages.git;openwrt-25.12#https://github.com/jkingben/build_openwrt.git;packages_immortalwrt_25#' feeds.conf.default
 echo "src-git easytier https://github.com/EasyTier/luci-app-easytier.git;main" >> feeds.conf.default
 echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5" >> feeds.conf.default
 echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> feeds.conf.default
